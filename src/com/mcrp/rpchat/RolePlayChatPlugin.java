bcfa2d22b6eb18fc06161d6fa7cfb721146f4eb8
Fozie <foziegameing@gmail.com>
2014-09-08 17:50:36 +0200

    Init
    Init of the project :D

---
+++ src/com/mcrp/rpchat/RolePlayChatPlugin.java
@@ -0,0 +1,15 @@
+package com.mcrp.rpchat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class RolePlayChatPlugin extends JavaPlugin {
+	
+	public void onEnable() {
+		
+	}
+	
+	public void onDisable() {
+		
+	}
+	
+}
